9bca066842c9508cff04862ec814338cc7a8c949
Grzegorz Ratajczak <Grzegorz.Ratajczak@asseco.pl>
2020-02-14 11:26:06 +0100

    mant-api add dependency for spring boot run

--- mant-api/src/main/java/mant/MantApiApplication.java
+++ mant-api/src/main/java/mant/MantApiApplication.java
@@ -1,4 +1,14 @@
 package mant;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class MantApiApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MantApiApplication.class, args);
+
+    }
 }
